ef86a734ebcaacf1b9177780a142c19af3f5cb8c
JoelMilla <jalejandro.milla@outlook.com>
2023-11-07 13:42:18 -0600

    Initial Commit

---
+++ Code Quiz/Code_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_QuizApp.swift
+//  Code Quiz
+//
+//  Created by Alumno on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Code_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
