7a4441fc4a489d5af6c44fb708a9b3129bba1a41
Joel-Milla <jalejandro.milla@outlook.com>
2023-11-10 10:25:00 -0600

    Modify GameViewModel for obtaining data from view

--- Code Quiz/ViewModels/GameViewModel.swift
+++ Code Quiz/ViewModels/GameViewModel.swift
@@ -5,4 +5,16 @@
 //  Created by Alumno on 10/11/23.
 //
 
-import Foundation
+import SwiftUI
+
+class GameViewModel: ObservableObject {
+    @Published private var game = Game()
+    
+    var currentQuestion: Question {
+        game.currentQuestion
+    }
+    
+    var questionProgressText: String {
+        "\(game.currentQuestionIndex) / \(game.numberOfQuestions)"
+    }
+}
